3efc3cd6a47ad71f6f3aaa024a1cd6b9ef0dce65
chikkam varshini <varshinichikkam@gmail.com>
2026-06-14 21:04:20 +0530

    Update CORS for Vercel

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://house-price-prediction-418u.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
